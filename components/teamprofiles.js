1cf09ae035c821142482c0177bea1c9001b3dcca
Spas <12554483+Spasmochi@users.noreply.github.com>
2020-07-31 15:45:21 +1000

    Added Teams and teamprofiles

---
+++ components/teamprofiles.js
@@ -0,0 +1,61 @@
+import React from "react";
+import Profile from "../components/profile";
+
+function teamprofiles() {
+  return (
+    <div>
+      <ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <Profile
+          memberPic="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+          memberName="Jane Cooper"
+          memberTitle="Paradigm Representative"
+          memberRole="Admin"
+        />
+        <Profile
+          memberPic="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+          memberName="Cody Fisher"
+          memberTitle="Lead Security Associate"
+          memberRole="Admin"
+        />
+        <Profile
+          memberPic="https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+          memberName="Esther Howard"
+          memberTitle="Assurance Administrator"
+          memberRole="Admin"
+        />
+        <Profile
+          memberPic="https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+          memberName="Jenny Wilson"
+          memberTitle="Chief Accountability Analyst"
+          memberRole="Admin"
+        />
+        <Profile
+          memberPic="https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+          memberName="Kristin Watson"
+          memberTitle="Investor Data Orchestrator"
+          memberRole="Admin"
+        />
+        <Profile
+          memberPic="https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+          memberName="Cameron Williamson"
+          memberTitle="Product Infrastructure Executive"
+          memberRole="Admin"
+        />
+        <Profile
+          memberPic="https://images.unsplash.com/photo-1534751516642-a1af1ef26a56?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+          memberName="Courtney Henry"
+          memberTitle="Investor Factors Associate"
+          memberRole="Admin"
+        />
+        <Profile
+          memberPic="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+          memberName="Theresa Webb"
+          memberTitle="Global Division Officer"
+          memberRole="Admin"
+        />
+      </ul>
+    </div>
+  );
+}
+
+export default teamprofiles;
--- pages/teams.js
+++ pages/teams.js
@@ -1,13 +1,13 @@
 import Sidebar from "../components/sidebar";
 import Head from "next/head";
-
+import TeamProfiles from "../components/teamprofiles";
 function teams() {
   return (
     <div>
       <Head>
         <title>Teams</title>
       </Head>
-      <Sidebar title="Teams" Content={[]} />
+      <Sidebar title="Teams" Content={[<TeamProfiles key="8" />]} />
     </div>
   );
 }
