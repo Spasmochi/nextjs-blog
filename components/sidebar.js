e6b69dd36df5eca4fb56ffe53f130a9546c8c43a
Spas <12554483+Spasmochi@users.noreply.github.com>
2020-08-02 02:50:04 +1000

    removes mx-auto from header

--- components/sidebar.js
+++ components/sidebar.js
@@ -522,7 +522,7 @@ function Sidebar(props) {
             tabIndex="0"
           >
             <div className="pt-2 pb-6 md:py-4">
-              <div className="max-w-7xl mx-auto mb-4 px-4 sm:px-6 md:px-6">
+              <div className="max-w-7xl mb-4 px-4 sm:px-6 md:px-6">
                 <h1 className="text-2xl font-semibold text-gray-900">
                   {props.title}
                 </h1>
