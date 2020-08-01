8003c4cf1006822c0176d5efb89fae8a186a0640
Spas <12554483+Spasmochi@users.noreply.github.com>
2020-08-02 01:38:15 +1000

    added head

--- pages/projects.js
+++ pages/projects.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Sidebar from "../components/sidebar";
 import Projectslist from "../components/projectslist";
 
@@ -6,6 +7,9 @@ import React from "react";
 function projects() {
   return (
     <div>
+      <Head>
+        <title>Projects</title>
+      </Head>
       <Sidebar title="Projects" content={[<Projectslist />]} />
     </div>
   );
